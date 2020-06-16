bb555e3d04f86d08276046731df6d220e984c8db
Leandro Lima <leulima@gmail.com>
2020-06-16 16:51:11 -0300

    Added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Leandro on 16/06/2020.
+//  Copyright © 2020 Gigaspace. All rights reserved.
+//
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
